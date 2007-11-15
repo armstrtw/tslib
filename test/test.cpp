2253dc73cff6ce09659268faa0cfd2790d6d10b0
whit <armstrong.whit@gmail.com>
2007-11-15 16:00:25 -0500

    added some tests for operator+
    1) will add more tests when initial debugging is done

--- test/test.cpp
+++ test/test.cpp
@@ -54,17 +54,59 @@ void set_colnames_test() {
   BOOST_CHECK_EQUAL(y.getColnames()==newColnames, 1);
 }
 
+void range_specifier_test() {
+
+  vector<int> x;
+  vector<int> y;
+
+  x.push_back(1);
+  x.push_back(2);
+  x.push_back(3);
+  x.push_back(4);
+  x.push_back(5);
+
+  y.push_back(1);
+  y.push_back(3);
+  y.push_back(5);
+
+  RangeSpecifier<int,size_t> r(&x[0],&y[0],x.size(),y.size());
+
+  BOOST_CHECK(r.getSize() == 3);
+  BOOST_CHECK(r.getArg1()[0] == 0);
+  BOOST_CHECK(r.getArg1()[1] == 2);
+  BOOST_CHECK(r.getArg1()[2] == 4);
+  
+  BOOST_CHECK(r.getArg2()[0] == 0);
+  BOOST_CHECK(r.getArg2()[1] == 1);
+  BOOST_CHECK(r.getArg2()[2] == 2);
+  
+  r.print();
+}
+
 void operators_test() {
-  TSDIM nr = 100;
+  TSDIM xnr = 100;
+  TSDIM ynr = 10;
   TSDIM nc = 10;
 
-  TSeries<double,double> x(nr,nc);
-  TSeries<double,double> y(nr,nc);
+  TSeries<double,double> x(xnr,nc);
+  TSeries<double,double> y(ynr,nc);
 
+  // gernate data
+  fill_n(x.getData(),x.nrow()*x.ncol(),100.0);
+  fill_n(y.getData(),y.nrow()*y.ncol(),1.0);
+
+  // generate dates
+  for(TSDIM xi = 0; xi < x.nrow(); xi++)
+    x.getDates()[xi] = xi;
+
+  // generate dates
+  for(TSDIM yi = 0; yi < y.nrow(); yi++)
+    y.getDates()[yi] = yi;
+    
   TSeries<double,double> z = x + y;
 
-  BOOST_CHECK_EQUAL( z.nrow(), 0 );
-  BOOST_CHECK_EQUAL( z.ncol(), 0 );
+  BOOST_CHECK_EQUAL( z.nrow(), y.nrow() );
+  BOOST_CHECK_EQUAL( z.ncol(), 10 );
 }
 
 
@@ -76,6 +118,7 @@ init_unit_test_suite( int argc, char* argv[] ) {
   test->add( BOOST_TEST_CASE( &std_constructor_test ) );
   test->add( BOOST_TEST_CASE( &set_colnames_test ) );
   
+  test->add( BOOST_TEST_CASE( &range_specifier_test ) );
   test->add( BOOST_TEST_CASE( &operators_test ) );
   return test;
 }
