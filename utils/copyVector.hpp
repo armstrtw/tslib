a638a71b7efca823442c10ac7c725535da869c17
whit <armstrong.whit@gmail.com>
2007-11-15 15:59:29 -0500

    made copyVector source address const

--- utils/copyVector.hpp
+++ utils/copyVector.hpp
@@ -1,28 +1,24 @@
 #ifndef COPY_VECTOR_HPP
 #define COPY_VECTOR_HPP
 
-
-
 template <typename T, typename U>
-void copyVector(T* dest, T* source, U len) {
+inline
+void copyVector(T* dest, const T* source, U len) {
   for(U i = 0; i < len; i++) {
     dest[i] = source[i];
   }
 }
 
-
-
 // specialized for builtins
 template <typename U>
 inline
-void copyVector(double* dest, double* source, U len) {
+void copyVector(double* dest, const double* source, U len) {
   memcpy(dest, source, len * sizeof(double) );
 }
 
-
 template <typename U>
 inline
-void copyVector(int* dest, int* source, U len) {
+void copyVector(int* dest, const int* source, U len) {
   memcpy(dest, source, len * sizeof(int) );
 }
 
