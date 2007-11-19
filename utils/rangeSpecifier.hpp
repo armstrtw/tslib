7790ca93585a4d1a661cc073716f6567d1325ba7
whit <armstrong.whit@gmail.com>
2007-11-19 08:11:22 -0500

    updated rangeSpecifier
    1) removed no argument constructor
    2) renamed arg1 and arg2 to index1 and index2
    3) appended _ to all private data members (except size which will be the next commit)

--- utils/rangeSpecifier.hpp
+++ utils/rangeSpecifier.hpp
@@ -10,123 +10,114 @@ using std::set_intersection;
 template<typename T, typename U>
 class RangeSpecifier {
 private:
-  T* dates;  // intersection dates
-  U* arg1;   // index of intersections of arg1
-  U* arg2;   // index of intersections of arg2
-  U size;    // also the length of all the internal vectors: dates, arg1, arg2
+  T* dates_;  // intersection dates_
+  U* index1_;   // index of intersections of index1_
+  U* index2_;   // index of intersections of index2_
+  U size;    // also the length of all the internal vectors: dates_, index1_, index2_
 
 public:
   ~RangeSpecifier();
   RangeSpecifier(const RangeSpecifier &r); // not allowed!!
-  RangeSpecifier();
-  RangeSpecifier(T *dates1,
-		 T *dates2,
-		 const U length_arg1,
-		 const U length_arg2);
+  RangeSpecifier(T *dates_1,
+		 T *dates_2,
+		 const U length_index1_,
+		 const U length_index2_);
 
-  const T* getDates();
-  const U* getArg1();
-  const U* getArg2();
-  const U getSize();
-  void print();
+  const T* getDates() const;
+  const U* getArg1() const;
+  const U* getArg2() const;
+  const U getSize() const;
+  void print() const;
   
   template<typename D, typename BinaryOpp>
-  void applyOpp(D* ans, D* arg1Data, D* arg2Data, BinaryOpp opp);
+  void applyOpp(D* ans, D* index1_Data, D* index2_Data, BinaryOpp opp) const;
 };
 
 template<typename T, typename U>
 RangeSpecifier<T,U>::~RangeSpecifier() {
-  delete []dates;
-  delete []arg1;
-  delete []arg2;
+  delete [] dates_;
+  delete [] index1_;
+  delete [] index2_;
 }
 
 template<typename T, typename U>
-RangeSpecifier<T,U>::RangeSpecifier() {
-  dates = NULL;
-  arg1 = NULL;
-  arg2 = NULL;
-  size = 0;
-}
-
-template<typename T, typename U>
-RangeSpecifier<T,U>::RangeSpecifier(T *dates1,
-                                    T *dates2,
-                                    const U length_arg1,
-                                    const U length_arg2) {
+RangeSpecifier<T,U>::RangeSpecifier(T *dates_1,
+                                    T *dates_2,
+                                    const U length_index1_,
+                                    const U length_index2_) {
 
   // find size of smaller of the two arguments
-  const U bufferSize = length_arg1 < length_arg2 ? length_arg1 : length_arg2;
+  const U bufferSize = length_index1_ < length_index2_ ? length_index1_ : length_index2_;
 
-  // alloc new buffer for dates, size is smallest of the two arguments
-  dates = new T[bufferSize];
+  // alloc new buffer for dates_, size is smallest of the two arguments
+  dates_ = new T[bufferSize];
 
   // if we can't get memory then print error and return NULL range specifier
-  if(dates==NULL) {
+  if(dates_==NULL) {
     cerr << "ERROR: RangeSpecifier" << endl;
     cerr << "out of memory" << endl;
     size = 0;
-    arg1 = NULL;
-    arg2 = NULL;
-    dates = NULL;
+    index1_ = NULL;
+    index2_ = NULL;
+    dates_ = NULL;
     return;
   }
 
   // find size of date intersection
-  T* dates_end =  set_intersection(dates1, dates1+length_arg1, dates2, dates2+length_arg2, dates);
+  T* dates_end =  set_intersection(dates_1, dates_1+length_index1_, dates_2, dates_2+length_index2_, dates_);
 
-  size = distance(dates,dates_end);
+  size = distance(dates_,dates_end);
 
   // if there is no intersection then specifier has no size, and elements should be set to null
-  // must delete buffer of dates that we allocated
+  // must delete buffer of dates_ that we allocated
   if(size==0) {
-    delete []dates;
-    arg1 = NULL;
-    arg2 = NULL;
-    dates = NULL;
+    delete []dates_;
+    index1_ = NULL;
+    index2_ = NULL;
+    dates_ = NULL;
   }
 
-  // since we have some dates in the intersection
+  // since we have some dates_ in the intersection
   // we can alloc space for the intersection points
-  arg1 = new U[size];
-  arg2 = new U[size];
+  index1_ = new U[size];
+  index2_ = new U[size];
 	
   // if we cannot get memory, then print error, release any memory that was allocated and return
-  if(arg1==NULL || arg2==NULL) {
+  if(index1_==NULL || index2_==NULL) {
     cerr << "ERROR: RangeSpecifier::RangeSpecifier" << endl;
     cerr << "out of memory" << endl;
-    delete []arg1;
-    delete []arg2;
-    delete []dates;
-    arg1 = NULL;
-    arg2 = NULL;
-    dates = NULL;
+    delete []index1_;
+    delete []index2_;
+    delete []dates_;
+    index1_ = NULL;
+    index2_ = NULL;
+    dates_ = NULL;
     size = 0;
     return;
   }
 
-  // placeholders to find intersecting dates
+  // placeholders to find intersecting dates_
   U date1_index = 0;
   U date2_index = 0;
   U dates_index = 0;
 
-  // go through all the dates in the intersection
+  // go through all the dates_ in the intersection
   while(dates_index < size) {
 
-    // catch up arg 1 to dates intersection
-    while(dates[dates_index] != dates1[date1_index]) {
+    // catch up arg 1 to dates_ intersection
+    while(dates_[dates_index] != dates_1[date1_index]) {
       date1_index++;
     }
 
-    // catch up arg 2 to dates intersection
-    while(dates[dates_index] != dates2[date2_index]) {
+    // catch up arg 2 to dates_ intersection
+    while(dates_[dates_index] != dates_2[date2_index]) {
       date2_index++;
     }
 
     // when equal, record position of matching elements
-    if(dates[dates_index] == dates1[date1_index] && dates[dates_index] == dates2[date2_index]) {
-      arg1[dates_index] = date1_index;
-      arg2[dates_index] = date2_index;
+    if(dates_[dates_index] == dates_1[date1_index] && dates_[dates_index] == dates_2[date2_index]) {
+      index1_[dates_index] = date1_index;
+      index2_[dates_index] = date2_index;
       dates_index++;
       date1_index++;
       date2_index++;
@@ -135,36 +126,36 @@ RangeSpecifier<T,U>::RangeSpecifier(T *dates1,
 }
 
 template<typename T, typename U>
-const U RangeSpecifier<T,U>::getSize() {
+const U RangeSpecifier<T,U>::getSize() const {
   return size;
 }
 
 template<typename T, typename U>
-const T* RangeSpecifier<T,U>::getDates() {
-  return dates;
+const T* RangeSpecifier<T,U>::getDates() const {
+  return dates_;
 }
 
 template<typename T, typename U>
-const U* RangeSpecifier<T,U>::getArg1() {
-  return arg1;
+const U* RangeSpecifier<T,U>::getArg1() const {
+  return index1_;
 }
 
 template<typename T, typename U>
-const U* RangeSpecifier<T,U>::getArg2() {
-  return arg2;
+const U* RangeSpecifier<T,U>::getArg2() const {
+  return index2_;
 }
 
 template<typename T, typename U>
 template<typename D, typename BinaryOpp>
-void RangeSpecifier<T,U>::applyOpp(D* ans, D* arg1Data, D* arg2Data, BinaryOpp opp) {
+void RangeSpecifier<T,U>::applyOpp(D* ans, D* index1_Data, D* index2_Data, BinaryOpp opp) const {
   for(U i = 0; i < size; i++)
-    ans[i] = opp( arg1Data[ arg1[i] ] , arg2Data[ arg2[i] ] );
+    ans[i] = opp( index1_Data[ index1_[i] ] , index2_Data[ index2_[i] ] );
 }
 
 template<typename T, typename U>
-void RangeSpecifier<T,U>::print() {
+void RangeSpecifier<T,U>::print() const {
   for(U i = 0; i < size; i++) {
-    cout << dates[i] << ":" << arg1[i] << ":" << arg2[i] << endl;
+    cout << dates_[i] << ":" << index1_[i] << ":" << index2_[i] << endl;
   }
 }
 
