60e62dfa42f9d6ce30ae30b03ba14eb8c8bcf69c
whit <armstrong.whit@gmail.com>
2007-11-20 14:00:09 -0500

    added numeric limits

---
+++ utils/numeric.traits.hpp
@@ -0,0 +1,28 @@
+#ifndef NUMERIC_TRAITS_HPP
+#define NUMERIC_TRAITS_HPP
+
+#include <limits>
+#include <cmath>
+
+template<typename T>
+class numeric_traits;
+
+template<>
+class numeric_traits<double> {
+public:
+  static const bool has_NA = true;
+  static double NA() {
+    return std::numeric_limits<double>::quiet_NaN();
+  }
+};
+
+template<>
+class numeric_traits<int> {
+public:
+  static const bool has_NA = true;
+  static int NA() {
+    return std::numeric_limits<int>::min();
+  }
+};
+
+#endif //NUMERIC_TRAITS_HPP
