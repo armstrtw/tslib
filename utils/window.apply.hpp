b76cc0de5a3d6ceb3b9ebe629c8a217c8315eb82
whit <armstrong.whit@gmail.com>
2007-11-20 14:09:25 -0500

    modified windowApply

--- utils/window.apply.hpp
+++ utils/window.apply.hpp
@@ -2,40 +2,39 @@
 #define WINDOW_APPLY_HPP
 
 #include "numeric.traits.hpp"
-using namespace std;
 
+template<typename ReturnType,
+         template<class> class F>
+class windowApply {
+public:
+  template<typename T, typename U>
+  static void apply(T ans, U beg, U end, const int window) {
+    T marker = beg;
 
-// assumes ans is same len as end - beg
-//template <template<class> class F>
-template <class T, template<class> class F>
-void windowApply(T* ans, T* beg, T* end, const int window) {
-  
-  //typename T* marker = beg;
-  T* marker = beg;
-
-  typedef typename sumTraits<T>::ReturnType ReturnType;
-
-  // if window is bigger than len, return all NA
-  if(static_cast<int>(distance(beg,end)) < window) {
+    // if window is bigger than len, return all NA
+    if(static_cast<int>(distance(beg,end)) < window) {
       while(beg != end) {
         *ans = numeric_traits<ReturnType>::NA();
-        ++beg;
         ++ans;
+        ++beg;
       }
       return;
-  }
+    }
 
-  // set 1st (N-1) to NA
-  for(int i = 0; i < (window-1); i++, beg++, ans++)
-    *ans = numeric_traits<ReturnType>::NA();
+    // set 1st (N-1) to NA
+    for(int i = 0; i < (window-1); i++, beg++, ans++) {
+      *ans = numeric_traits<ReturnType>::NA();
+    }
 
-  // apply fun to rest
-  while(beg != end) {
-    *ans = F<T>()(marker,beg);
-    ++beg;
-    ++marker;
-    ++ans;
+    // apply fun to rest
+    while(beg != end) {
+      *ans = F<ReturnType>::apply(marker,beg);
+      ++beg;
+      ++marker;
+      ++ans;
+    }
   }
-}
+};
+
 
 #endif // WINDOW_APPLY_HPP
