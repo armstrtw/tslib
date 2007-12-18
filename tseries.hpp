52cc735ccfe12d61b0ac83a5353e8e8a7a294254
whit <armstrong.whit@gmail.com>
2007-12-18 12:27:25 -0500

    fixed type error in tseries::window(int) method

--- tseries.hpp
+++ tseries.hpp
@@ -409,7 +409,7 @@ const TSeries<TDATE,ReturnType,TSDIM,TSDATABACKEND,DatePolicy> TSeries<TDATE,TDA
   // set new colnames
   ans.setColnames(getColnames());
 
-  TDATA* ans_data = ans.getData();
+  ReturnType* ans_data = ans.getData();
   TDATA* data = getData();
 
   for(TSDIM col = 0; col < ncol(); col++) {
