0cbd8b3547b35cd222ac354b82b75c29262628b2
whit <armstrong.whit@gmail.com>
2008-01-06 10:18:11 -0500

    added prototypes for correlation and covariance

--- tslib/tseries.hpp
+++ tslib/tseries.hpp
@@ -28,6 +28,23 @@ namespace tslib {
 template<typename TDATE, typename TDATA, typename TSDIM, template<typename,typename,typename> class TSDATABACKEND, template<typename> class DatePolicy> class TSeries;
 
 template<typename TDATE, typename TDATA, typename TSDIM, template<typename,typename,typename> class TSDATABACKEND, template<typename> class DatePolicy>
+TSeries<TDATE,TDATA,TSDIM,TSDATABACKEND,DatePolicy> correlation(const TSeries<TDATE,TDATA,TSDIM,TSDATABACKEND,DatePolicy>& lhs,
+                                                                const TSeries<TDATE,TDATA,TSDIM,TSDATABACKEND,DatePolicy>& rhs,
+                                                                const TSDIM periods);
+
+template<typename TDATE, typename TDATA, typename TSDIM, template<typename,typename,typename> class TSDATABACKEND, template<typename> class DatePolicy>
+TSeries<TDATE,TDATA,TSDIM,TSDATABACKEND,DatePolicy> covariance(const TSeries<TDATE,TDATA,TSDIM,TSDATABACKEND,DatePolicy>& lhs,
+                                                                const TSeries<TDATE,TDATA,TSDIM,TSDATABACKEND,DatePolicy>& rhs,
+                                                                const TSDIM periods);
+
+  /*
+    template<typename TDATE, typename TDATA, typename TSDIM, template<typename,typename,typename> class TSDATABACKEND, template<typename> class DatePolicy, typename F >
+    TSeries<TDATE,TDATA,TSDIM,TSDATABACKEND,DatePolicy> applyWindowIntersection(const TSeries<TDATE,TDATA,TSDIM,TSDATABACKEND,DatePolicy>& lhs,
+    const TSeries<TDATE,TDATA,TSDIM,TSDATABACKEND,DatePolicy>& rhs,
+    const U periods);
+  */
+
+template<typename TDATE, typename TDATA, typename TSDIM, template<typename,typename,typename> class TSDATABACKEND, template<typename> class DatePolicy>
 TSeries<TDATE,TDATA,TSDIM,TSDATABACKEND,DatePolicy> operator+(const TSeries<TDATE,TDATA,TSDIM,TSDATABACKEND,DatePolicy>& lhs,
                                                               const TSeries<TDATE,TDATA,TSDIM,TSDATABACKEND,DatePolicy>& rhs);
 
