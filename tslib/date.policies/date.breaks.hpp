5df93d12d653d1081157c55bfa15d6dcf40d62d5
whit <armstrong.whit@gmail.com>
2008-01-15 14:24:38 -0500

    added MonthlyBreaks and WeeklyBreaks

--- tslib/date.policies/date.breaks.hpp
+++ tslib/date.policies/date.breaks.hpp
@@ -17,11 +17,9 @@ void QuarterlyBreaks(T beg, T end, COLL<TSDIM>& coll) {
   TSDIM i = 0;
 
   while(beg != (end - 1) ) {
-    //cout << DatePolicy<value_type>::month(*beg) << endl;
     // end of quarter
     if(DatePolicy<value_type>::month(*beg) % 3 == 0
-       &&
-       // diffferent month or different year is next
+       && // and diffferent month or different year is next
        (DatePolicy<value_type>::month(*beg) != DatePolicy<value_type>::month(*next) ||
         DatePolicy<value_type>::year(*beg) != DatePolicy<value_type>::year(*next) )
        )
@@ -35,6 +33,61 @@ void QuarterlyBreaks(T beg, T end, COLL<TSDIM>& coll) {
   // last element is always in
   back_inserter(coll) = i;
 }
+
+template<template<typename> class DatePolicy,
+         typename TSDIM,
+         typename T,
+         template<typename U, typename = std::allocator<U> > class COLL>
+void MonthlyBreaks(T beg, T end, COLL<TSDIM>& coll) {
+  typedef typename std::iterator_traits<T>::value_type value_type;
+
+  T next = beg + 1;
+  TSDIM i = 0;
+
+  while(beg != (end - 1) ) {
+
+    if(// diffferent month or different year is next
+       DatePolicy<value_type>::month(*beg) != DatePolicy<value_type>::month(*next) ||
+       DatePolicy<value_type>::year(*beg) != DatePolicy<value_type>::year(*next) )
+      {
+        back_inserter(coll) = i;
+      }
+    ++i;
+    ++beg;
+    ++next;
+  }
+  // last element is always in
+  back_inserter(coll) = i;
+}
+
+template<template<typename> class DatePolicy,
+         typename TSDIM,
+         typename T,
+         template<typename U, typename = std::allocator<U> > class COLL>
+void WeeklyBreaks(T beg, T end, COLL<TSDIM>& coll) {
+  typedef typename std::iterator_traits<T>::value_type value_type;
+
+  T next = beg + 1;
+  TSDIM i = 0;
+
+  while(beg != (end - 1) ) {
+
+    if(
+       // day of week > next day of week
+       DatePolicy<value_type>::dayofweek(*beg) > DatePolicy<value_type>::dayofweek(*next) ||
+       // because dates can be sparse, if we travel more than 7 days, then it's a new week
+       DatePolicy<value_type>::daily_distance(*beg, *next) >= 7
+       ) {
+        back_inserter(coll) = i;
+      }
+    ++i;
+    ++beg;
+    ++next;
+  }
+  // last element is always in
+  back_inserter(coll) = i;
+}
+
 
 }  // namespace tslib
 
