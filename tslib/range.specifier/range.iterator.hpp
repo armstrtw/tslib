977ec7739263746d371c537be57cf0b29ac820ee
whit <armstrong.whit@gmail.com>
2008-01-17 11:14:17 -0500

    added iterator_traits to RangeIterator class

--- tslib/range.specifier/range.iterator.hpp
+++ tslib/range.specifier/range.iterator.hpp
@@ -5,12 +5,25 @@ namespace tslib {
 
 
 #include <iterator>
+
+
 template<typename DATAPOINTER, typename INDEXPOINTER>
 class RangeIterator {
 private:
   const DATAPOINTER data_pointer_;
   INDEXPOINTER index_pointer_;
 public:
+  typedef std::random_access_iterator_tag iterator_category;
+  typedef typename std::iterator_traits<DATAPOINTER>::value_type value_type;
+  typedef value_type* pointer;
+  typedef const value_type* const_pointer;
+  typedef value_type* iterator;
+  typedef const value_type* const_iterator;
+  typedef value_type& reference;
+  typedef const value_type& const_reference;
+  typedef std::size_t size_type;
+  typedef std::ptrdiff_t difference_type;
+
   RangeIterator(const DATAPOINTER data_pointer, const INDEXPOINTER index_pointer);
   RangeIterator(const RangeIterator& r);
 
@@ -22,10 +35,22 @@ public:
   RangeIterator<DATAPOINTER,INDEXPOINTER>& operator+=(const int i);
   RangeIterator<DATAPOINTER,INDEXPOINTER>& operator-=(const int i);
 
-  RangeIterator<DATAPOINTER,INDEXPOINTER>& operator+(const int i) const;
-  RangeIterator<DATAPOINTER,INDEXPOINTER>& operator-(const int i) const;
+  RangeIterator<DATAPOINTER,INDEXPOINTER> operator+(const int i) const;
+  RangeIterator<DATAPOINTER,INDEXPOINTER> operator-(const int i) const;
 
   typename std::iterator_traits<DATAPOINTER>::value_type operator*();
+
+  friend bool operator==(RangeIterator& lhs, RangeIterator& rhs) {
+    return (lhs.data_pointer_ == rhs.data_pointer_) && (lhs.index_pointer_ == rhs.index_pointer_);
+  }
+
+  friend bool operator!=(RangeIterator& lhs, RangeIterator& rhs) {
+    return (lhs.data_pointer_ != rhs.data_pointer_) || (lhs.index_pointer_ != rhs.index_pointer_);
+  }
+
+  friend difference_type operator-(RangeIterator& lhs, RangeIterator& rhs) {
+    return (lhs.index_pointer_ - rhs.index_pointer_);
+  }
 };
 
 template<typename DATAPOINTER, typename INDEXPOINTER>
@@ -87,13 +112,13 @@ RangeIterator<DATAPOINTER,INDEXPOINTER>& RangeIterator<DATAPOINTER,INDEXPOINTER>
 
 template<typename DATAPOINTER, typename INDEXPOINTER>
 inline
-RangeIterator<DATAPOINTER,INDEXPOINTER>& RangeIterator<DATAPOINTER,INDEXPOINTER>::operator+(const int i) const {
+RangeIterator<DATAPOINTER,INDEXPOINTER> RangeIterator<DATAPOINTER,INDEXPOINTER>::operator+(const int i) const {
   return RangeIterator(data_pointer_, index_pointer_ + i);
 }
 
 template<typename DATAPOINTER, typename INDEXPOINTER>
 inline
-RangeIterator<DATAPOINTER,INDEXPOINTER>& RangeIterator<DATAPOINTER,INDEXPOINTER>::operator-(const int i) const {
+RangeIterator<DATAPOINTER,INDEXPOINTER> RangeIterator<DATAPOINTER,INDEXPOINTER>::operator-(const int i) const {
   return RangeIterator(data_pointer_, index_pointer_ - i);
 }
 
