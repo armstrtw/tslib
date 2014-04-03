832feff4b37301136b556dace060d8918a4b8f93
whit <armstrong.whit@gmail.com>
2014-04-03 14:48:10 -0400

    remove std::ostream& operator<< from RangeSpecifier

--- tslib/range.specifier/rangeSpecifier.hpp
+++ tslib/range.specifier/rangeSpecifier.hpp
@@ -132,14 +132,6 @@ namespace tslib {
     return index2_;
   }
 
-
-  template<typename T, typename U>
-  std::ostream& operator<< (std::ostream& os, const RangeSpecifier<T,U>& rs) {
-    for(U i = 0; i < rs.size_; i++) {
-      os << rs.dates_[i] << ":" << rs.index1_[i] << ":" << rs.index2_[i] << std::endl;
-    }
-    return os;
-  }
 } // namespace tslib
 
 #endif
